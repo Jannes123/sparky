d3d82d826d97a5dbcf4557cf589102530cd91036
Jannes123 <jannes.engelbrecht2@gmail.com>
2018-10-01 13:04:02 +0200

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,9 +3,17 @@ sudo /bin/mkdir /var/log/sparky
 sudo /usr/bin/touch /var/log/sparky/debug.log
 sudo chmod -R ugo+rw /var/log/sparky
 
+sudo ufw allow 44444
+sudo ufw allow 7070
+sudo apt  -y install default-libmysqlclient-dev mysql-server
+
+# CREATE USER django;
+# CREATE DATABASE DB1 CHARACTER SET utf8;
+# GRANT ALL PRIVILEGES ON DB1.* TO 'django'@'localhost' WITH GRANT OPTION;
 
 virtualenv -p python2 testa
 source testa/bin/activate
+
 
 pip install -r requirements.txt
 
@@ -14,5 +22,10 @@ pip install -r requirements.txt
 # ufw allow 7070
 ./manage.py runserver 0.0.0.0:7070 &
 
+sleep 2s
+
 python server.py &
+
+sleep 2s
+
 python atm.py
